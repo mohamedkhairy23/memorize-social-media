a5638d8af67fc0413804fa0008fb8bc335fccf84
Mohamed Khairy <mohamedkhairy17696@gmail.com>
2023-06-12 18:46:32 +0300

    fetch Posts

--- frontend/src/components/Posts/Post/Post.js
+++ frontend/src/components/Posts/Post/Post.js
@@ -1,7 +1,77 @@
-import React from "react";
+import "./Post.css";
+import {
+  Card,
+  CardActions,
+  CardContent,
+  Button,
+  Typography,
+  CardMedia,
+} from "@mui/material";
+import ThumbUpAltIcon from "@mui/icons-material/ThumbUpAlt";
+import DeleteIcon from "@mui/icons-material/Delete";
+import MoreHorizIcon from "@mui/icons-material/MoreHoriz";
+import moment from "moment";
 
-const Post = () => {
-  return <div>Post</div>;
+const Post = ({ post }) => {
+  return (
+    <Card
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "space-between",
+        borderRadius: "15px",
+        height: "100%",
+        position: "relative",
+      }}
+    >
+      <CardMedia
+        height="0"
+        sx={{
+          pt: "56.25%",
+          backgroundColor: "rgba(0,0,0,0.5)",
+          backgroundBlendMode: "darken",
+        }}
+        image={post.selectedFile}
+        title={post.title}
+      />
+      <div className="overlay1">
+        <Typography variant="h6">{post.creator}</Typography>
+        <Typography variant="h6">{moment(post.createdAt).fromNow()}</Typography>
+      </div>
+      <div className="overlay2">
+        <Button style={{ color: "white" }} size="small" onClick={() => {}}>
+          <MoreHorizIcon fontSize="default" />
+        </Button>
+      </div>
+      <div className="details">
+        <Typography variant="body2" color="textSecondary">
+          {post.tags.map((tag) => `#${tag} `)}
+        </Typography>
+      </div>
+      <CardContent>
+        <Typography sx={{ padding: "0 16px" }} variant="h5" gutterBottom>
+          {post.message}
+        </Typography>
+      </CardContent>
+      <CardActions
+        sx={{
+          padding: "0 16px 8px 16px",
+          display: "flex",
+          justifyContent: "space-between",
+        }}
+      >
+        <Button size="small" color="primary" onClick={() => {}}>
+          <ThumbUpAltIcon fontSize="small" />
+          Like
+          {post.likeCount}
+        </Button>
+        <Button size="small" color="primary" onClick={() => {}}>
+          <DeleteIcon fontSize="small" />
+          Delete
+        </Button>
+      </CardActions>
+    </Card>
+  );
 };
 
 export default Post;
--- frontend/src/components/Posts/Posts.js
+++ frontend/src/components/Posts/Posts.js
@@ -1,14 +1,25 @@
-import React from "react";
+import { Grid, CircularProgress } from "@mui/material";
 import Post from "./Post/Post";
 import { useSelector } from "react-redux";
+
 const Posts = () => {
   const posts = useSelector((state) => state.posts);
   console.log(posts);
-  return (
-    <div>
-      <h1>Posts</h1>
-      <Post />
-    </div>
+  return !posts.length ? (
+    <CircularProgress />
+  ) : (
+    <Grid
+      sx={{ display: "flex", alignItems: "center" }}
+      container
+      alignItems="stretch"
+      spacing={3}
+    >
+      {posts.map((post) => (
+        <Grid key={post._id} item xs={12} sm={6} md={6}>
+          <Post post={post} />
+        </Grid>
+      ))}
+    </Grid>
   );
 };
 
